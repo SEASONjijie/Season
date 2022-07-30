9574b4aa591fac9b89e908e8d2fb0ad851b32bae
Season <jijielonglong@yeah.net>
2022-07-31 02:29:30 +0800

    i change

--- hello season/src/hello.java
+++ hello season/src/hello.java
@@ -2,7 +2,7 @@
 public class hello {
 	  public static void main(String[] args) {
 		  String myName="Season";
-		  System.out.println(myName);
+		  System.out.println(myNaswdfawsdhfoasme);
 		
 	  }
 }
